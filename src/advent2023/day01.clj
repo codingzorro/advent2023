40a1878fa924ebd79a41267a2e107f77046cb86e
codingzorro <48889997+codingzorro@users.noreply.github.com>
2024-01-14 11:22:48 +0100

    create environment for Day 1

--- src/advent2023/day01.clj
+++ src/advent2023/day01.clj
@@ -1,4 +1,4 @@
-(ns advent2023.core)
+(ns advent2023.day01)
 
 (defn foo
   "I don't do a whole lot."
---
+++ test/advent2023/day01_test.clj
@@ -0,0 +1,7 @@
+(ns advent2023.day01-test
+  (:require [clojure.test :refer :all]
+            [advent2023.day01 :refer :all]))
+
+(deftest a-test
+  (testing "if 1 is 1"
+    (is (= 1 1))))
