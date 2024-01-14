2fb6db243198efecc3e6513672bfcb4a9eb02ec8
codingzorro <48889997+codingzorro@users.noreply.github.com>
2024-01-14 20:57:10 +0100

    Implment functions needed for morning of Day 1

--- src/advent2023/day01.clj
+++ src/advent2023/day01.clj
@@ -1,5 +1,12 @@
 (ns advent2023.day01)
 
+(defn- as-number
+  "convert [1 2] into 12"
+  [[a b]]
+  (+ (* a 10) b))
+
+
+
 (defn decode
   "extract digits in a string; e.g. \"a1b2\" --> [1 2]"
   [a-string]
@@ -11,4 +18,6 @@
   "Create a two-digit integer using the first and last elements of a given
   array of digits"
   [an-array]
-  42)
+  (->> an-array
+    (#(vector (first %) (last %)))
+    as-number))
